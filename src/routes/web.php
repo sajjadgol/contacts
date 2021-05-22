317009184993fa0f5a9c2c2365a3232299743200
sajjad <sajjad.golchin@gmail.com>
2021-05-22 12:03:49 +0430

    bugfix

--- src/routes/web.php
+++ src/routes/web.php
@@ -6,10 +6,3 @@ Route::get("test" , function(){
     return 'form package';
 });
 
-Route::get("test1" , function(){
-    return "test1";
-});
-
-Route::get("test2" , function(){
-   return "test2";
-});
