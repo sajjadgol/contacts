89a4baa144068e112a3313f24ac7a5a0dd03fcfd
sajjad <sajjad.golchin@gmail.com>
2021-05-22 10:09:19 +0430

    start package and add service provider and route

---
+++ src/ContactProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Gol\Contact;
+
+
+
+use Illuminate\Support\ServiceProvider;
+
+class  ContactProvider extends  ServiceProvider
+{
+    public function register()
+    {
+    }
+
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__. '/routes/web.php');
+
+    }
+}
